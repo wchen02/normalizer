8cb807b18fdd6240a313ba696fa21b00a05aff52
Wensheng Chen <icydragoon@gmail.com>
2019-07-23 00:56:09 -0700

    feat: Add hiding log based on log level set in ENV, add clean script

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const fileType = require('file-type');
-const stream = require('stream');
-
+const log = require('loglevel');
 const { promisify } = require('util')
 const download = require('download');
 const loadJsonFile = require('load-json-file');
@@ -14,42 +13,50 @@ const DATA_DIR = 'data/';
 const getDataFile = (filename) => DATA_DIR + filename;
 
 async function downloadDataFileGallery(dataFilename) {
+    log.info(`Opening ${dataFilename}`)
     const jsonFilePath = getDataFile(dataFilename);
     const data = await loadJsonFile(jsonFilePath);
+    log.info(JSON.stringify(data));
 
     if (data.gallery.length) {
+        log.info(`Found ${data.gallery.length} gallery images`);
         await Promise.all(data.gallery.map(async (imgUrl) => {
             // mkdir with ':' and '/' chars replaced to '_'
             const galleryPath = DOWNLOAD_DIR + data.url.replace(/[:\\/]/g, '_').toLowerCase();
+            log.info(`Making directory ${galleryPath}`);
             mkdirp.sync(galleryPath);
             
+            log.info(`Download image ${imgUrl}`);
             const fileData = await download(imgUrl);
             const fileDataType = fileType(fileData);
 
             if (!fileDataType) {
-                console.warn('Invalid filetype, skipping.');
+                log.warn('Invalid filetype, skipping.');
                 return;
             }
 
             if (fileDataType.mime.includes('image/')) {
                 const writeFileAsync = promisify(fs.writeFile);
-                const filename = uuid() + '.' + fileDataType.ext;
-                writeFileAsync(galleryPath + '/' + filename, fileData);
-                console.log(`Downloaded ${filename}`);
+                const filename = galleryPath + '/' + uuid() + '.' + fileDataType.ext;
+                writeFileAsync(filename, fileData);
+                log.log(`Downloaded to ${filename}`);
             }
         }));
     }
 }
 
 async function main() {
+    log.setLevel(process.env.LOG_LEVEL);
     const readdirAsync = promisify(fs.readdir)
 
     let files;
 
     try {
+        log.info(`Scaning data directory ${DATA_DIR}`);
         files = await readdirAsync(DATA_DIR);
+        log.info(`Found ${files.length} data files.`);
     } catch (err) {
-        return console.error('Unable to scan directory: ' + err);
+        return log.error('Unable to scan directory: ' + err);
     }
 
     await Promise.all(files.map(file => downloadDataFileGallery(file)));
